fe90602bfa618c153f7419ebdc2095385dd03645
Markus Tiller <mti011@uit.no>
2021-01-21 09:33:12 +0100

    Specify interface to ComputeGraph class, without implementaiton

--- mod.py
+++ mod.py
@@ -37,3 +37,27 @@ class DifferentiableOperation(Operation, DifferentiableTensor):
     """Abstract base class for operations in a graph that can be used to automatically generate a graph to compute gradients"""
     pass
 
+
+class ComputeGraph:
+    """Class to represent a collection of nodes that create a compute graph"""
+
+    def __init__(self, node_in_graph):
+        """Creates a ComputeGraph object that finds all nodes in the graph by traversing the graph."""
+        raise NotImplementedError
+
+    def prune(self, required_outputs):
+        """Creates a new compute graph that only contains the nodes required to compute the nodes in required_outputs"""
+        raise NotImplementedError
+
+    def with_gradients(self, output_scalar_node, gradient_targets):
+        """
+        Returns a (ComputeGraph, dict) tuple where the ComputeGraph is a new graph that contains gradients.
+        output_scalar_node is a node that evaluates to a scalar value, which is the value being differentiated.
+        gradient_targets is a list of nodes that output_scalar_node is beind differentiated with respect to.
+        The dict in the return value is a map from gradient_target: gradient_node, for each gradient_target in gradient_targets.
+        """
+        raise NotImplementedError
+
+    def evaluate(self, context=None, required_outputs=None):
+        """Evaluates the the graph. required_outputs can be specified as a subset of nodes that are required to be evaluated"""
+        raise NotImplementedError
