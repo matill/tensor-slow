f1dca87259d051e7b10494ea4d6b633a87bcea79
Markus Tiller <mti011@uit.no>
2021-01-26 12:14:59 +0100

    Add Input and Variable types

---
+++ tensorslow/tensor/input.py
@@ -0,0 +1,15 @@
+import numpy as np
+from .core import Tensor
+
+
+class Input(Tensor):
+    """Node with optionally strict shape that is evaluated according to a value that is set in the context dictionary"""
+
+    def evaluate(self, context):
+        assert self in context, f"Attempted to evaluate graph without specifying value of Input node: {self.to_dict(context)}"
+        value = context[self]
+        if self.shape != None:
+            assert self.shape == value.shape, f"Input node {self.to_dict(context)} expected value of shape {self.shape} != {value.shape}"
+
+        return value
+
---
+++ tensorslow/tensor/variable.py
@@ -0,0 +1,39 @@
+from .core import Tensor
+import numpy as np
+
+
+# TODO: Some room for optimization with respect to memory management using more complex numpy functions.
+class Variable(Tensor):
+    """
+    Evaluates to a fixed numpy array, but with the ability to update value, without changing shape.
+    Similar to the Constant class, but the ComputeGraph API will be able to find all Variable types in
+    the graph and update them when running gradient descent.
+    """
+
+    def __init__(self, np_array):
+        super().__init__(np_array.shape)
+        self.val = np_array
+
+    def evaluate(self, context):
+        return self.val
+
+    def set_val(self, np_array):
+        assert self.shape == np_array.shape
+        self.val = np_array
+
+    def increment(self, np_array):
+        assert self.shape == np_array.shape
+        self.val += np_array
+
+    def decrement(self, np_array):
+        assert self.shape == np_array.shape
+        self.val += np_array
+
+    def scale(self, alpha):
+        alpha = float(alpha)
+        self.val *= alpha
+
+    def scaled_increment(self, np_array, alpha):
+        alpha = float(alpha)
+        assert self.shape == np_array.shape
+        self.val += np_array * alpha
